edc9c618c0d2bc3f45616f41cb71f51f2e810ef1
Ken Nagamine <7955031+nag8@users.noreply.github.com>
2020-03-08 15:21:31 +0900

    実装

---
+++ src/main.rb
@@ -0,0 +1,18 @@
+require 'time'
+def timer(arg, &proc)
+  x = case arg
+  when Numeric then arg
+  when Time    then arg - Time.now
+  when String  then Time.parse(arg) - Time.now
+  else raise   end
+  sleep x if block_given?
+  yield
+end
+
+timer("12:50") do 
+  system("say '昼会を行いましょう！'")
+end
+
+timer("12:59") do 
+  system("say '今日もがんばりましょう！'")
+end
